e513fba19914012f6d682596cfd5eb305baa5040
WuTao88 <wutao.88@qq.com>
2018-07-12 19:51:08 +0800

    Initial commit

---
+++ src/main/java/com/configserver/configServerApplication.java
@@ -0,0 +1,15 @@
+package com.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public  class configServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(configServerApplication.class,args);
+    }
+}
